9808c10fe2332f5c8a4a8cf7c82808ec0f5cd828
Jiyun Lee <selena.jy.lee@gmail.com>
2020-11-04 15:58:22 +0000

    test

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# site
+# My site
+
+## To Do
+
+> ### Nav
+
+- [X] Adjust gaps
+- [X] Set the minimum & maximum width
+- [X] Set the link scroll position
+
+> ### Main
+
+- [ ] Create the Typo Animation
+- [x] Set the scroll action
+
+> ### It's me
+
+- [ ] Add contents
+
+> ### Experience
+
+- [ ] Change words
+- [ ] Add styles
+- [ ] Adjust gaps
+- [ ] Add periods & contents
+
+> ### Projects
+
+- [ ] Add contents
+- [ ] Add images
+- [ ] Add styles
+
+> ### Contact me
+
+- [ ] github & linkedIn & email
+
+> ### Skills
+
+- [ ] Add page
+- [ ] Add Icons
---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+// const main = document.querySelector(".js-main"),
+//     greetings = main.querySelectorAll(".js-main__text");
+
+// const greetingMessages = [];
+// let idx = 0;
+
+// function init() {
+//     // setGreetingMessages();
+// }
+
+// function setGreetingMessages() {
+    
+//     greetings.forEach(p => {
+//         const {textContent} = p;
+//         greetingMessages.push(textContent.split(""));
+//     });
+
+//     greetings.forEach(p => {
+//         p.textContent = "";
+//     });
+
+//     console.log(greetings);
+//     console.log(greetingMessages);
+//     typingText(greetingMessages[0]);
+// }
+
+// function typingText(word) {
+    
+//     // setInterval(typing, 1000);
+// }
+
+// function typing(){
+//     const orgin = greetings[0].textContent;
+//     console.log(orgin);
+//     // greetings[0].textContent = orgin + greetingMessages[0][idx];
+//     // console.log(greetings[0].textContent);
+// }
+
+// init();
